30163a87705ca91a591c690a0a19e0d825d6361f
npcprogrammer101 <wangtcau@gmail.com>
2025-11-30 19:05:07 +1100

    Organised Files

---
+++ app/(root)/sorting/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SortingAlgo = () => {
+  return (
+    <div>SortingAlgo</div>
+  )
+}
+
+export default SortingAlgo
---
+++ components/hello.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+const Hello = () => {
+  console.log("I am client");
+  return <div>Hello</div>;
+};
+
+export default Hello;
